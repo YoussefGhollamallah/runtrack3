73334a1365f6b2a9debdc8b7dd8053c45a9fd7a1
YoussefGhollamallah <youssef.gh@live.fr>
2024-09-09 13:48:32 +0200

    correction faute de frappe

--- jour01/job07/script.js
+++ jour01/job07/script.js
@@ -1,16 +1,16 @@
 function jourtravaille(date) {
     const joursFeries2020 = [
-        new Date(2020, 0, 1),  // Jour de l'An (1er janvier)
-        new Date(2020, 3, 13), // Lundi de Pâques (13 avril)
-        new Date(2020, 4, 1),  // Fête du Travail (1er mai)
-        new Date(2020, 4, 8),  // Victoire 1945 (8 mai)
-        new Date(2020, 4, 21), // Ascension (21 mai)
-        new Date(2020, 5, 1),  // Lundi de Pentecôte (1er juin)
-        new Date(2020, 6, 14), // Fête Nationale (14 juillet)
-        new Date(2020, 7, 15), // Assomption (15 août)
-        new Date(2020, 10, 1), // Toussaint (1er novembre)
-        new Date(2020, 10, 11),// Armistice (11 novembre)
-        new Date(2020, 11, 25) // Noël (25 décembre)
+        new Date(2020, 1, 1),  // Jour de l'An (1er janvier)
+        new Date(2020, 4, 13), // Lundi de Pâques (13 avril)
+        new Date(2020, 5, 1),  // Fête du Travail (1er mai)
+        new Date(2020, 5, 8),  // Victoire 1945 (8 mai)
+        new Date(2020, 5, 21), // Ascension (21 mai)
+        new Date(2020, 6, 1),  // Lundi de Pentecôte (1er juin)
+        new Date(2020, 7, 14), // Fête Nationale (14 juillet)
+        new Date(2020, 8, 15), // Assomption (15 août)
+        new Date(2020, 11, 1), // Toussaint (1er novembre)
+        new Date(2020, 11, 11),// Armistice (11 novembre)
+        new Date(2020, 12, 25) // Noël (25 décembre)
     ];
 
     let estFerie = joursFeries2020.some(jourFerie =>
@@ -35,5 +35,5 @@ function jourtravaille(date) {
 }
 
 
-let dateTest = new Date(2020, 4, 1); 
+let dateTest = new Date(2020, 5, 1); 
 jourtravaille(dateTest);
