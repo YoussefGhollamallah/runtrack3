76541ce2013cb1643592f40f974aa774357ca406
YoussefGhollamallah <youssef.gh@live.fr>
2024-09-11 14:49:31 +0200

    connexion à la base de données et récupére tous les users

---
+++ jour04/job04/users.php
@@ -0,0 +1,30 @@
+<?php
+
+header("Content-Type: application/json");
+
+
+$db_user = "root";
+$db_password = "root";
+$dsn = "mysql:host=localhost; dbname=utilisateurs;charset=utf8";
+
+try {
+
+    $dbconnect = new PDO($dsn,$db_user,$db_password);
+
+    $dbconnect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+    $stmt = $dbconnect("SELECT * FROM  utilisateurs");
+    $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    echo json_encode($users);
+
+} catch (PDOException $e) {
+
+    echo json_encode([" error " => "connexion à la database échoué " . $e->getMessage() ]);
+}
+
+
+$dbconnect = null;
+
+?>
