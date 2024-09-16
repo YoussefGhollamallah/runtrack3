9778d2df3e6715df66ce09182aa0c218f340e4eb
YoussefGhollamallah <youssef.gh@live.fr>
2024-09-16 13:35:18 +0200

    job 3 terminé

---
+++ jour06/job03/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <header class="container-fluid mb-1 mt-1 row" style="height: 150px;">
+        <nav class="col-xxl w-100 bg-primary h-100"></nav>
+    </header>
+    <main>
+        <section class="container-fluid mb-1 gap-1 row" style="height: 300px;">
+            <article class="col h-100 bg-secondary"></article>
+            <article class="col h-100 bg-secondary"></article>
+            <article class="col h-100 bg-secondary"></article>
+        </section>
+        <section class="container-fluid gap-1 row" style="height: 200px;">
+            <article class="col-8 h-100 bg-primary"></article>
+            <article class="col h-100 bg-primary"></article>
+        </section>
+    </main>
+    <footer class="container-fluid row gap-1 mb-2 mt-1" style="height: 300px;">
+        <article class="col bg-success h-25 align-self-start"></article>
+        <article class="col bg-success h-25 align-self-center"></article>
+        <article class="col bg-success h-25 align-self-end"></article>
+    </footer>
+</body>
+</html>
